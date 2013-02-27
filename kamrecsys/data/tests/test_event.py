c5854f2b38a71b48103339188dce3e3c6a05f30e
Toshihiro Kamishima <mail@kamishima.net>
2013-02-28 08:10:57 +0900

    update nosetests

--- kamrecsys/data/tests/test_event.py
+++ kamrecsys/data/tests/test_event.py
@@ -5,18 +5,20 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from numpy.testing import (assert_array_equal, assert_array_less,
+                           assert_allclose, assert_array_max_ulp,
+                           assert_array_almost_equal_nulp)
 import unittest
+import os
+import numpy as np
 
 ##### Test Classes #####
 
 class TestEventUtilMixin(unittest.TestCase):
 
     def test_to_eid_event(self):
-        import os
-        import numpy as np
-        from kamrecsys.data import EventWithScoreData
-        from kamrecsys.datasets import SAMPLE_PATH
+        from .. import EventWithScoreData
+        from ...datasets import SAMPLE_PATH
 
         infile = os.path.join(SAMPLE_PATH, 'pci.event')
         dtype = np.dtype([('event', 'S18', 2), ('score', np.float)])
@@ -37,10 +39,8 @@ class TestEventUtilMixin(unittest.TestCase):
         assert_array_equal(x['event'], check)
 
     def test_to_iid_event(self):
-        import os
-        import numpy as np
-        from kamrecsys.data import EventWithScoreData
-        from kamrecsys.datasets import SAMPLE_PATH
+        from .. import EventWithScoreData
+        from ...datasets import SAMPLE_PATH
 
         infile = os.path.join(SAMPLE_PATH, 'pci.event')
         dtype = np.dtype([('event', 'S18', 2), ('score', np.float)])
