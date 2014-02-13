b16527533177069cc2b796698e5cf61275a73a93
Toshihiro Kamishima <mail@kamishima.net>
2014-02-14 01:47:32 +0900

    __init__

--- kamrecsys/datasets/__init__.py
+++ kamrecsys/datasets/__init__.py
@@ -4,10 +4,11 @@
 Sample Data Sets
 """
 
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import (
+    print_function,
+    division,
+    absolute_import,
+    unicode_literals)
 
 #==============================================================================
 # Imports
@@ -15,4 +16,5 @@ from __future__ import unicode_literals
 
 from ._base import *
 from ._movielens import *
+from ._sushi3 import *
 from ._others import *
