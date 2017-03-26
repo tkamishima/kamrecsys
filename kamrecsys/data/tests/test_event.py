891f3fd6dff7c021bdbf1fa3916245a63318873e
Toshihiro Kamishima <mail@kamishima.net>
2017-03-27 02:45:23 +0900

    data.EventWithScoreData.filter_event -> test

--- kamrecsys/data/tests/test_event.py
+++ kamrecsys/data/tests/test_event.py
@@ -120,6 +120,21 @@ class TestEventWithScoreData(unittest.TestCase):
         digitized_scores = data.digitize_score(np.linspace(1.0, 5.0, 9))
         assert_array_equal(digitized_scores, np.arange(9))
 
+    def test_filter_event(self):
+        data = load_movielens_mini()
+
+        data.filter_event(data.score > 3)
+        assert_allclose(
+            data.score,
+            [4., 4., 4., 5., 4., 5., 5., 5., 4., 5.,
+             4., 5., 5., 4., 5., 4., 4., 4., 4., 4., 4.])
+
+        assert_allclose(
+            data.to_eid(0, data.event[:, 0]),
+            [10, 5, 10, 1, 7, 7, 9, 7, 10, 1,
+             2, 1, 7, 6, 7, 6, 1, 9, 1, 6, 10])
+
+
 # =============================================================================
 # Main Routines
 # =============================================================================
