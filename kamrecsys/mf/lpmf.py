e6546f8ec57a893b9c9c9289858466ccea590cbc
Toshihiro Kamishima <mail@kamishima.net>
2017-03-27 05:43:52 +0900

    Logistic Probabilistic Matrix Factorization: EventItemFinder

---
+++ kamrecsys/mf/lpmf.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Matrix Factorization: logistic probabilistic matrix factorization model
+"""
+
+from __future__ import (
+    print_function,
+    division,
+    absolute_import)
+from six.moves import xrange
+
+# =============================================================================
+# Module metadata variables
+# =============================================================================
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+import logging
+import sys
+import numpy as np
+from scipy.optimize import fmin_cg
+from sklearn.utils import check_random_state
+
+from ..recommenders import BaseEventItemFinder
+
+# =============================================================================
+# Public symbols
+# =============================================================================
+
+__all__ = ['EventItemFinder']
+
+# =============================================================================
+# Constants
+# =============================================================================
+
+# =============================================================================
+# Module variables
+# =============================================================================
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+
+class EventItemFinder(BaseEventItemFinder):
+    """
+    A probabilistic matrix factorization model proposed in [1]_.
+    A method of handling bias terms is defined by equation (5) in [2]_.
+
+    Parameters
+    ----------
+    C : float, optional
+        regularization parameter (= :math:`\lambda`), default=1.0
+    k : int, optional
+        the number of latent factors (= sizes of :math:`\mathbf{p}_u` or
+        :math:`\mathbf{q}_i`), default=1
+    tol : optional, float
+        tolerance parameter for optimizer
+    maxiter : int, default=200
+        maximum number of iterations is maxiter times the number of parameters
+
+    Attributes
+    ----------
+    mu_ : array_like
+        global bias
+    bu_ : array_like
+        users' biases
+    bi_ : array_like
+        items' biases
+    p_ : array_like
+        latent factors of users
+    q_ : array_like
+        latent factors of items
+    i_loss_ : float
+        the loss value after initialization
+    f_loss_ : float
+        the loss value after fitting
+    opt_outputs_ : tuple
+        extra outputs of an optimizer
+
+    Notes
+    -----
+    Preference scores are modeled by the sum of bias terms and the cross
+    product of users' and items' latent factors with L2 regularizers.
+
+    .. math::
+
+        \hat{y} =
+        \sum_{(u,i) \in \mathcal{D}}
+        \mu + b_u + c_i + \mathbf{p}_u^\top \mathbf{q}_i
+        + \lambda (\|P_u\|_2^2 + \|Q_u\|_2^2
+        + \|\mathbf{b}\|_2^2 + \|\mathbf{c}\|_2^2)
+
+    For computational reasons, a loss term is scaled by the number of
+    events, and a regularization term is scaled by the number of model
+    parameters.
+
+    References
+    ----------
+    .. [1] R. Salakhutdinov and A. Mnih. "Probabilistic matrix factorization"
+        NIPS 20
+    .. [2] Y. Koren, "Factorization Meets the Neighborhood: A Multifaceted
+        Collaborative Filtering Model", KDD2008
+    """
+
+    def __init__(self, C=1.0, k=1, tol=None, maxiter=200, random_state=None):
+        super(EventItemFinder, self).__init__(random_state=random_state)
+
+        self.C = np.float(C)
+        self.k = np.int(k)
+        self.tol = tol
+        self.maxiter = maxiter
+        self.mu_ = None
+        self.bu_ = None
+        self.bi_ = None
+        self.p_ = None
+        self.q_ = None
+        self.i_loss_ = np.inf
+        self.f_loss_ = np.inf
+        self.opt_outputs_ = None
+
+        # private instance variables
+        self._coef = None
+        self._dt = None
+
+    def _init_coef(self, ev, sc, n_objects):
+        """
+        Initialize model parameters
+
+        Parameters
+        ----------
+        ev : array, shape(n_events, 2)
+            event data
+        sc : array, shape(n_events,)
+            scores attached to events
+        n_objects : array, shape(2,)
+            vector of numbers of objects
+        """
+        # constants
+        n_events = ev.shape[0]
+        n_users = n_objects[0]
+        n_items = n_objects[1]
+        k = self.k
+
+        # define dtype for parameters
+        self._dt = np.dtype([
+            ('mu', np.float, (1,)),
+            ('bu', np.float, n_users),
+            ('bi', np.float, n_items),
+            ('p', np.float, (n_users, k)),
+            ('q', np.float, (n_items, k))
+        ])
+
+        # memory allocation
+        self._coef = np.zeros(1 + n_users + n_items +
+                              n_users * k + n_items * k,
+                              dtype=float)
+
+        # set array's view
+        self.mu_ = self._coef.view(self._dt)['mu'][0]
+        self.bu_ = self._coef.view(self._dt)['bu'][0]
+        self.bi_ = self._coef.view(self._dt)['bi'][0]
+        self.p_ = self._coef.view(self._dt)['p'][0]
+        self.q_ = self._coef.view(self._dt)['q'][0]
+
+        # set bias term
+        self.mu_[0] = np.sum(sc) / n_events
+        for i in xrange(n_users):
+            j = np.nonzero(ev[:, 0] == i)[0]
+            if len(j) > 0:
+                self.bu_[i] = np.sum(sc[j] - self.mu_[0]) / len(j)
+        for i in xrange(n_items):
+            j = np.nonzero(ev[:, 1] == i)[0]
+            if len(j) > 0:
+                self.bi_[i] = (
+                    np.sum(sc[j] - (self.mu_[0] + self.bu_[ev[j, 0]])) /
+                    len(j))
+
+        # fill cross terms by normal randoms whose s.d.'s are mean residuals
+        var = 0.0
+        for i in xrange(n_events):
+            var += (
+                (sc[i] -
+                 (self.mu_[0] + self.bu_[ev[i, 0]] + self.bi_[ev[i, 1]])) ** 2)
+        var /= n_events
+        self.p_[0:n_users, :] = (
+            self._rng.normal(0.0, np.sqrt(var), (n_users, k)))
+        self.q_[0:n_items, :] = (
+            self._rng.normal(0.0, np.sqrt(var), (n_items, k)))
+
+        # scale a regularization term by the number of parameters
+        self._reg = self.C / (1 + (k + 1) * (n_users + n_items))
+
+    def loss(self, coef, ev, sc, n_objects):
+        """
+        loss function to optimize
+
+        Parameters
+        ----------
+        coef : array_like, shape=(variable,)
+            coefficients of this model
+        ev : array_like, shape(n_events, 2), dtype=int
+            user and item indexes
+        sc : array_like, shape(n_events,), dtype=float
+            target scores
+        n_objects : array_like, shape(2,), dtype=int
+            numbers of users and items
+
+        Returns
+        -------
+        loss : float
+            value of loss function
+        """
+        # constants
+        n_events = ev.shape[0]
+
+        # set array's view
+        mu = coef.view(self._dt)['mu'][0]
+        bu = coef.view(self._dt)['bu'][0]
+        bi = coef.view(self._dt)['bi'][0]
+        p = coef.view(self._dt)['p'][0]
+        q = coef.view(self._dt)['q'][0]
+
+        # loss term
+        esc = (mu[0] + bu[ev[:, 0]] + bi[ev[:, 1]] +
+               np.sum(p[ev[:, 0], :] * q[ev[:, 1], :], axis=1))
+        loss = np.sum((sc - esc) ** 2)
+
+        # regularization term
+        reg = (np.sum(bu ** 2) + np.sum(bi ** 2) +
+               np.sum(p ** 2) + np.sum(q ** 2))
+
+        return loss / n_events + self._reg * reg
+
+    def grad_loss(self, coef, ev, sc, n_objects):
+        """
+        gradient of loss function
+
+        Parameters
+        ----------
+        coef : array_like, shape=(variable,)
+            coefficients of this model
+        ev : array_like, shape(n_events, 2), dtype=int
+            user and item indexes
+        sc : array_like, shape(n_events,), dtype=float
+            target scores
+        n_objects : array_like, shape(2,), dtype=int
+            numbers of users and items
+
+        Returns
+        -------
+        grad : array_like, shape=coef.shape
+            the first gradient of loss function by coef
+        """
+        # constants
+        n_events = ev.shape[0]
+        n_users = n_objects[0]
+        n_items = n_objects[1]
+
+        # set input array's view
+        mu = coef.view(self._dt)['mu'][0]
+        bu = coef.view(self._dt)['bu'][0]
+        bi = coef.view(self._dt)['bi'][0]
+        p = coef.view(self._dt)['p'][0]
+        q = coef.view(self._dt)['q'][0]
+
+        # create empty gradient
+        grad = np.zeros_like(coef)
+        grad_mu = grad.view(self._dt)['mu'][0]
+        grad_bu = grad.view(self._dt)['bu'][0]
+        grad_bi = grad.view(self._dt)['bi'][0]
+        grad_p = grad.view(self._dt)['p'][0]
+        grad_q = grad.view(self._dt)['q'][0]
+
+        # gradient of loss term
+        neg_res = -(sc - (mu[0] + bu[ev[:, 0]] + bi[ev[:, 1]] +
+                          np.sum(p[ev[:, 0], :] * q[ev[:, 1], :], axis=1)))
+        grad_mu[0] = np.sum(neg_res)
+        grad_bu[:] = np.bincount(ev[:, 0], weights=neg_res,
+                                 minlength=n_users)
+        grad_bi[:] = np.bincount(ev[:, 1], weights=neg_res,
+                                 minlength=n_items)
+        weights = neg_res[:, np.newaxis] * q[ev[:, 1], :]
+        for i in xrange(self.k):
+            grad_p[:, i] = np.bincount(ev[:, 0], weights=weights[:, i],
+                                       minlength=n_users)
+        weights = neg_res[:, np.newaxis] * p[ev[:, 0], :]
+        for i in xrange(self.k):
+            grad_q[:, i] = np.bincount(ev[:, 1], weights=weights[:, i],
+                                       minlength=n_items)
+
+        # re-scale gradients
+        grad[:] = grad[:] / n_events
+
+        # gradient of regularization term
+        grad_bu[:] += self._reg * bu
+        grad_bi[:] += self._reg * bi
+        grad_p[:, :] += self._reg * p
+        grad_q[:, :] += self._reg * q
+
+        return grad
+
+    def fit(self, data, user_index=0, item_index=1, score_index=0, tol=None,
+            maxiter=None, random_state=None, **kwargs):
+        """
+        fitting model
+
+        Parameters
+        ----------
+        data : :class:`kamrecsys.data.EventWithScoreData`
+            data to fit
+        user_index : optional, int
+            Index to specify the position of a user in an event vector.
+            (default=0)
+        item_index : optional, int
+            Index to specify the position of a item in an event vector.
+            (default=1)
+        score_index : optional, int
+            Ignored if score of data is a single criterion type. In a multi-
+            criteria case, specify the position of the target score in a score
+            vector. (default=0)
+        random_state: RandomState or an int seed (None by default)
+            A random number generator instance. If None is given, the
+            object's random_state is used
+        kwargs : keyword arguments
+            keyword arguments passed to optimizers
+        """
+
+        # set random state
+        if random_state is None:
+            random_state = self.random_state
+        self._rng = check_random_state(random_state)
+
+        # get input data
+        ev, sc, n_objects = \
+            self._get_event_and_score(data,
+                                      (user_index, item_index),
+                                      score_index)
+
+        # initialize coefficients
+        self._init_coef(ev, sc, n_objects)
+
+        # check optimization parameters
+        if 'disp' not in kwargs:
+            kwargs['disp'] = False
+        if 'gtol' in kwargs:
+            del kwargs['gtol']
+        if self.tol is not None:
+            kwargs['gtol'] = self.tol
+        if maxiter is None:
+            kwargs['maxiter'] = int(self.maxiter * self._coef.shape[0])
+        else:
+            kwargs['maxiter'] = int(maxiter * self._coef.shape[0])
+
+        # get final loss
+        self.i_loss_ = self.loss(self._coef, ev, sc, n_objects)
+
+        # optimize model
+        # fmin_bfgs is slow for large data, maybe because due to the
+        # computation cost for the Hessian matrices.
+        res = fmin_cg(self.loss,
+                      self._coef,
+                      fprime=self.grad_loss,
+                      args=(ev, sc, n_objects),
+                      full_output=True,
+                      **kwargs)
+
+        # get parameters
+        self._coef[:] = res[0]
+        self.f_loss_ = res[1]
+        self.opt_outputs_ = res[2:]
+
+        # add parameters for unknown users and items
+        self.mu_ = self._coef.view(self._dt)['mu'][0].copy()
+        self.bu_ = np.r_[self._coef.view(self._dt)['bu'][0], 0.0]
+        self.bi_ = np.r_[self._coef.view(self._dt)['bi'][0], 0.0]
+        self.p_ = np.r_[self._coef.view(self._dt)['p'][0],
+                        np.zeros((1, self.k), dtype=np.float)]
+        self.q_ = np.r_[self._coef.view(self._dt)['q'][0],
+                        np.zeros((1, self.k), dtype=np.float)]
+
+        # clean up temporary instance variables
+        del self._coef
+        del self._reg
+        del self._dt
+        del self._rng
+
+    def raw_predict(self, ev):
+        """
+        predict score of given one event represented by internal ids
+
+        Parameters
+        ----------
+        (user, item) : array_like
+            a target user's and item's ids. unknown objects assumed to be
+            represented by n_object[event_otype]
+
+        Returns
+        -------
+        sc : float
+            score for a target pair of user and item
+
+        Raises
+        ------
+        TypeError
+            shape of an input array is illegal
+        """
+
+        return (self.mu_[0] + self.bu_[ev[:, 0]] + self.bi_[ev[:, 1]] +
+                np.sum(self.p_[ev[:, 0], :] * self.q_[ev[:, 1], :],
+                       axis=1))
+
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# =============================================================================
+# Module initialization
+# =============================================================================
+
+# init logging system ---------------------------------------------------------
+logger = logging.getLogger('kamrecsys')
+if not logger.handlers:
+    logger.addHandler(logging.NullHandler())
+
+# =============================================================================
+# Test routine
+# =============================================================================
+
+
+def _test():
+    """ test function for this module
+    """
+
+    # perform doctest
+    import sys
+    import doctest
+
+    doctest.testmod()
+
+    sys.exit(0)
+
+# Check if this is call as command script -------------------------------------
+
+if __name__ == '__main__':
+    _test()
---
+++ kamrecsys/mf/tests/test_lpmf.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    print_function,
+    division,
+    absolute_import,
+    unicode_literals)
+from six.moves import xrange
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+from numpy.testing import (
+    assert_array_equal,
+    assert_array_less,
+    assert_allclose,
+    assert_array_max_ulp,
+    assert_array_almost_equal_nulp)
+import unittest
+
+import os
+import numpy as np
+
+# =============================================================================
+# Module variables
+# =============================================================================
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# =============================================================================
+# Test Classes
+# =============================================================================
+
+
+class TestEventItemFinder(unittest.TestCase):
+
+    def test_loss(self):
+        from kamrecsys.datasets import load_movielens100k
+        from kamrecsys.mf.lpmf import EventItemFinder
+
+        # setup
+        data = load_movielens100k(
+            os.path.join(os.path.dirname(__file__), 'mlmini_t.event'))
+        rec = EventItemFinder(C=0.1, k=2, tol=1e-03, random_state=1234)
+
+        # tev, tsc, n_objects = \
+        #     rec._setup_data(data, trg, n_trgvars=2,
+        #                     user_index=0, item_index=1, score_index=0)
+        # rec._init_coef(data, tev, tsc, n_objects, tol=1000.0, maxiter=200)
+        #
+        # # set array's view
+        # mu = rec._coef.view(rec._dt)['mu']
+        # bu = rec._coef.view(rec._dt)['bu']
+        # bi = rec._coef.view(rec._dt)['bi']
+        # p = rec._coef.view(rec._dt)['p']
+        # q = rec._coef.view(rec._dt)['q']
+        #
+        # # all zero
+        # mu[:] = 0.0
+        # bu[:] = 0.0
+        # bi[:] = 0.0
+        # p[:, :] = 0.0
+        # q[:, :] = 0.0
+        # self.assertAlmostEqual(rec.loss(rec._coef, tev, tsc, n_objects),
+        #                        15.811193562306155)
+        #
+        # # all one
+        # mu[:] = 1.0
+        # bu[:] = 1.0
+        # bi[:] = 1.0
+        # p[:, :] = 1.0
+        # q[:, :] = 1.0
+        # self.assertAlmostEqual(rec.loss(rec._coef, tev, tsc, n_objects),
+        #                        2.4876784107910042)
+        #
+        # mu[0] = 1.0
+        # mu[1] = 1.5
+        # bu[0][:] = np.arange(0.0, 0.8, 0.1)
+        # bu[1][:] = np.arange(0.2, 1.0, 0.1)
+        # bi[0][:] = np.arange(0.0, 1.0, 0.1) + 1.0
+        # bi[1][:] = np.arange(0.0, 1.0, 0.1) + 2.0
+        # p[0][:, 0] = 0.5
+        # p[0][:, 1] = 1.0
+        # p[1][:, 0] = 1.0
+        # p[1][:, 1] = 1.0
+        # q[0][:, 0] = 0.2
+        # q[0][:, 1] = 1.0
+        # q[1][:, 0] = 1.0
+        # q[1][:, 1] = 1.0
+        # self.assertAlmostEqual(rec.loss(rec._coef, tev, tsc, n_objects),
+        #                        186.54355756518166)
+
+    def test_grad_loss(self):
+        from kamrecsys.datasets import load_movielens100k
+        from kamrecsys.mf.lpmf import EventItemFinder
+
+        # setup
+        data = load_movielens100k(
+            os.path.join(os.path.dirname(__file__), 'mlmini_t.event'))
+        rec = EventItemFinder(C=0.1, k=2, tol=1e-03, random_state=1234)
+
+        # tev, tsc, n_objects = \
+        #     rec._setup_data(data, trg, n_trgvars=2,
+        #                     user_index=0, item_index=1, score_index=0)
+        # rec._init_coef(data, tev, tsc, n_objects, tol=1000.0, maxiter=200)
+
+        # # set array's view
+        # mu = rec._coef.view(rec._dt)['mu']
+        # bu = rec._coef.view(rec._dt)['bu']
+        # bi = rec._coef.view(rec._dt)['bi']
+        # p = rec._coef.view(rec._dt)['p']
+        # q = rec._coef.view(rec._dt)['q']
+        #
+        # # all zero
+        # mu[:] = 0.0
+        # bu[:] = 0.0
+        # bi[:] = 0.0
+        # p[:, :] = 0.0
+        # q[:, :] = 0.0
+        # grad = rec.grad_loss(rec._coef, tev, tsc, n_objects)
+        # assert_allclose(grad[:5],
+        #                 [-1.86666667, -0.76666667, -0.13333333,
+        #                  -0.23333333, -0.13333333])
+        # assert_allclose(grad[-5:],
+        #                 [0., 0., 0., 0., 0.])
+
+        # # all one
+        # mu[:] = 1.0
+        # bu[:] = 1.0
+        # bi[:] = 1.0
+        # p[:, :] = 1.0
+        # q[:, :] = 1.0
+        # grad = rec.grad_loss(rec._coef, tev, tsc, n_objects)
+        # assert_allclose(grad[:8],
+        #                 [4.66666667e-01, 2.33424242e-01, 3.34242424e-02,
+        #                  1.00090909e-01, 3.34242424e-02, 9.09090909e-05,
+        #                  9.09090909e-05, 9.09090909e-05])
+        # assert_allclose(grad[-8:],
+        #                 [0.26675758, 0.26675758, 0.16675758, 0.16675758,
+        #                  0.06675758, 0.06675758, 0.20009091, 0.20009091])
+
+        # mu[0] = 1.0
+        # mu[1] = 1.5
+        # bu[0][:] = np.arange(0.0, 0.8, 0.1)
+        # bu[1][:] = np.arange(0.2, 1.0, 0.1)
+        # bi[0][:] = np.arange(0.0, 1.0, 0.1) + 1.0
+        # bi[1][:] = np.arange(0.0, 1.0, 0.1) + 2.0
+        # p[0][:, 0] = 0.5
+        # p[0][:, 1] = 1.0
+        # p[1][:, 0] = 1.0
+        # p[1][:, 1] = 1.0
+        # q[0][:, 0] = 0.2
+        # q[0][:, 1] = 1.0
+        # q[1][:, 0] = 1.0
+        # q[1][:, 1] = 1.0
+        # grad = rec.grad_loss(rec._coef, tev, tsc, n_objects)
+        # assert_allclose(grad[:8],
+        #                 [-3.036162e+01, 1.805286e+01, 7.528713e+00,
+        #                  6.039861e+00, 1.494799e+00, -1.060639e+01,
+        #                  4.545455e-05, -2.267424e+01],
+        #                 rtol=1e-5)
+        # assert_allclose(grad[-8:],
+        #                 [22.285565, 22.285565, 27.127236, 27.127236,
+        #                  -19.282996, -19.282996, 1.63885, 1.63885],
+        #                 rtol=1e-5)
+
+        # mu[0] = 2.0
+        # mu[1] = 3.5
+        # bu[0][:] = np.arange(0.8, 0.0, -0.1)
+        # bu[1][:] = np.arange(0.0, 0.8, 0.1) * 2
+        # bi[0][:] = np.arange(0.0, 1.0, 0.1) * 1.5 + 1.0
+        # bi[1][:] = np.arange(0.0, 1.0, 0.1) + 2.0
+        # p[0][:, 0] = np.arange(0.0, 0.8, 0.1) * 0.8 + 3
+        # p[0][:, 1] = 1.0
+        # p[1][:, 0] = 1.0
+        # p[1][:, 1] = np.arange(0.8, 0.0, -0.1) * 0.5 + 1
+        # q[0][:, 0] = np.arange(1.0, 0.0, -0.1) * 0.3 + 2
+        # q[0][:, 1] = np.arange(0.0, 1.0, 0.1)
+        # q[1][:, 0] = np.arange(1.0, 0.0, -0.1)
+        # q[1][:, 1] = np.arange(0.0, 1.0, 0.1) * 0.4 - 1
+        # grad = rec.grad_loss(rec._coef, tev, tsc, n_objects)
+        # assert_allclose(grad[:8],
+        #                 [2.294263e+01, 2.365823e+01, 1.206014e+01,
+        #                  1.584586e+01, 7.741774e+00, -6.048586e+00,
+        #                  2.727273e-05, -1.748194e+01],
+        #                 rtol=1e-5)
+        # assert_allclose(grad[-8:],
+        #                 [-17.932169, -17.355552, 3.694892, 5.874205,
+        #                  -9.412818, -8.337879, 5.433757, 8.25851],
+        #                 rtol=1e-5)
+
+    def test_class(self):
+        from kamrecsys.datasets import load_movielens_mini
+        from kamrecsys.mf.lpmf import EventItemFinder
+
+        # setup
+        data = load_movielens_mini()
+        rec = EventItemFinder(C=0.1, k=2, tol=1e-03, random_state=1234)
+
+        self.assertDictEqual(
+            vars(rec),
+            {'C': 0.1, 'n_otypes': 0, 'bu_': None, 'bi_': None, 'k': 2,
+             'p_': None, 'q_': None, '_coef': None, 'f_loss_': np.inf,
+             'iid': None, 'i_loss_': np.inf, 'eid': None, 'tol': 1e-03,
+             'n_objects': None, '_dt': None, 'mu_': None, 'opt_outputs_': None,
+             'maxiter': 200, 'random_state': 1234, '_rng': None})
+
+        # recommender.fit(data, disp=False)
+        # self.assertAlmostEqual(recommender.i_loss_,
+        #                        0.74652578358324106, delta=1e-5)
+        # self.assertAlmostEqual(recommender.f_loss_,
+        #                        0.025638738121075231, delta=1e-5)
+        #
+        # self.assertAlmostEqual(recommender.predict((1, 7)),
+        #                        3.9873641434545979, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((1, 9)),
+        #                        4.9892118821609106, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((1, 11)),
+        #                        3.6480799850368273, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((3, 7)),
+        #                        3.6336318795279228, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((3, 9)),
+        #                        4.2482001235634943, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((3, 11)),
+        #                        3.7236984083417841, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((5, 7)),
+        #                        3.4141968145802597, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((5, 9)),
+        #                        3.9818882049478654, delta=1e-5)
+        # self.assertAlmostEqual(recommender.predict((5, 11)),
+        #                        3.4710520150321895, delta=1e-5)
+        # x = np.array([
+        #     [1, 7], [1, 9], [1, 11],
+        #     [3, 7], [3, 9], [3, 11],
+        #     [5, 7], [5, 9], [5, 11]])
+        # assert_allclose(
+        #     recommender.predict(x),
+        #     [3.98736414, 4.98921188, 3.64807999, 3.63363188, 4.24820012,
+        #      3.72369841, 3.41419681, 3.9818882, 3.47105202],
+        #     rtol=1e-5)
+
+# =============================================================================
+# Main Routines
+# =============================================================================
+
+if __name__ == '__main__':
+    unittest.main()
