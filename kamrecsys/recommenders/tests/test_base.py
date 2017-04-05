9a76e2e09adb03f8830f135c713ef0c40326afcf
Toshihiro Kamishima <mail@kamishima.net>
2017-04-06 05:50:03 +0900

    test BaseRecommender, BaseEventRecommender

--- kamrecsys/recommenders/tests/test_base.py
+++ kamrecsys/recommenders/tests/test_base.py
@@ -22,7 +22,10 @@ from numpy.testing import (
     assert_array_max_ulp,
     assert_array_almost_equal_nulp)
 
-from kamrecsys.recommenders import BaseEventRecommender
+import numpy as np
+
+from kamrecsys.datasets import load_movielens_mini
+from kamrecsys.recommenders import BaseRecommender, BaseEventRecommender
 
 # =============================================================================
 # Variables
@@ -37,24 +40,83 @@ from kamrecsys.recommenders import BaseEventRecommender
 # =============================================================================
 
 
+class Recommender(BaseRecommender):
+
+    def predict(self, eev):
+        pass
+
 class EventRecommender(BaseEventRecommender):
 
     def __init__(self):
-        super(EventRecommender, self).__init__(random_state=1234)
+        super(EventRecommender, self).__init__()
 
-    def raw_predict(self):
-        pass
+    def raw_predict(self, ev):
+
+        return(np.zeros(ev.shape[0]))
 
 # =============================================================================
 # Test Classes
 # =============================================================================
 
 
+class TestBaseRecommender(TestCase):
+
+    def test_class(self):
+
+        # setup
+        data = load_movielens_mini()
+
+        # __init__()
+        rec = Recommender(random_state=1234)
+
+        self.assertEqual(rec.n_otypes, 0)
+        self.assertIsNone(rec.n_objects)
+        self.assertIsNone(rec.eid)
+        self.assertIsNone(rec.iid)
+        self.assertEqual(rec.random_state, 1234)
+        self.assertIsNone(rec._rng)
+        self.assertDictEqual(rec.fit_results_, {})
+
+        # _set_object_info()
+        rec._set_object_info(data)
+
+        self.assertEqual(rec.n_otypes, 2)
+        assert_array_equal(rec.n_objects, [8, 10])
+        assert_array_equal(rec.eid, data.eid)
+        assert_array_equal(rec.iid, data.iid)
+
+        # to_eid()
+        self.assertEqual(rec.to_eid(0, 0), 1)
+        self.assertEqual(rec.to_eid(1, 0), 1)
+
+        # to_iid()
+        self.assertEqual(rec.to_iid(0, 1), 0)
+        self.assertEqual(rec.to_iid(1, 1), 0)
+
+
 class TestBaseEventRecommender(TestCase):
 
     def test_class(self):
-        pass
 
+        # setup
+        data = load_movielens_mini()
+
+        rec = EventRecommender()
+        rec._set_object_info(data)
+
+        # _set_object_info
+        rec._set_event_info(data)
+
+        assert_array_equal(rec.event_otypes, [0, 1])
+        self.assertEqual(rec.s_event, 2)
+
+        # predict
+        self.assertEqual(rec.predict([0, 0]).ndim, 0)
+        self.assertEqual(rec.predict([[0, 0]]).ndim, 0)
+        self.assertEqual(rec.predict([[0, 0], [0, 1]]).ndim, 1)
+        assert_array_equal(rec.predict([[0, 0], [0, 1]]).shape, (2,))
+        with self.assertRaises(TypeError):
+            rec.predict([[0]])
 
 # =============================================================================
 # Main Routine
