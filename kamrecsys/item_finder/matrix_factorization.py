088506640e73b4b3b538df0c39071b5b3b835fd0
Toshihiro Kamishima <mail@kamishima.net>
2017-12-24 00:41:30 +0900

    fix: initialization of implicit PMF

--- kamrecsys/item_finder/matrix_factorization.py
+++ kamrecsys/item_finder/matrix_factorization.py
@@ -557,10 +557,8 @@ class ImplicitLogisticPMF(BaseImplicitItemFinder):
         self.bi_[:] = ev.sum(axis=0).ravel() / n_users
 
         # fill cross terms by normal randoms
-        mask = np.bincount(ev[:, 0], minlength=n_users).nonzero()[0]
-        self.p_[mask, :] = self._rng.normal(0.0, 1.0, (len(mask), k))
-        mask = np.bincount(ev[:, 1], minlength=n_items).nonzero()[0]
-        self.q_[mask, :] = self._rng.normal(0.0, 1.0, (len(mask), k))
+        self.p_[0:n_users, :] = (self._rng.normal(0.0, 1.0, (n_users, k)))
+        self.q_[0:n_items, :] = (self._rng.normal(0.0, 1.0, (n_items, k)))
 
         # scale a regularization term by the number of parameters
         self._reg = self.C / (1 + (k + 1) * (n_users + n_items))
