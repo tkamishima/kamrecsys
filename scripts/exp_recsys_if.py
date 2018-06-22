47e953a1f87d69a1f6f0041b6dfd66d662c4464c
Toshihiro Kamishima <mail@kamishima.net>
2018-06-22 14:37:57 +0900

    rename script

--- scripts/exp_recsys_if.py
+++ scripts/exp_recsys_if.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Experimentation script for Item Finders with Explicit Ratings
+Experimentation script for Item Finders
 
 Input Format
 ------------
